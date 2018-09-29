f0e56ad11fb1614de20546c248adcbc41ee7d8dd
peng xiao hui <1527194200@qq.com>
2018-09-29 22:36:25 +0800

    dev 2 test

--- learn.py
+++ learn.py
@@ -1,6 +1,6 @@
 print "hello world you"
 print "dev  2"
-print "hello world me"
-print "me"
-print 'you'
-print 'you and me'
+print "hello world me 2 "
+print "me 2 "
+print 'you 2'
+print 'you and me 2'
